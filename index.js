48379a426382293588c53c74abf719c3fef6924c
sathishd_recode <sathishkumar.durairaj@kamerai.ai>
2025-05-02 12:26:40 +0530

    code changes

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const { Temporal } = require("temporal-polyfill");
+
+import * as ZonedDateTime from "temporal-polyfill/fns/zoned-date-time.js";
+
 const reverseString = (str) => {
     let reversedStr = '';
     for(let i= str.length -1; i>=0; i--){
@@ -47,6 +51,38 @@ const testData = [
     { name: 'Alice', age: 20 }
 ];
 
+// console.log(
+//     testData.groupBy(item => item.age)
+// );
+
+const rotateArray = (arr, k) => {
+    const n = arr.length;
+    k = k % n; // Handle cases where k is greater than n
+    return arr.slice(n - k).concat(arr.slice(0, n - k));
+}
+console.log(rotateArray([1, 2, 3, 4, 5,6,7], 3)); // [4, 5, 1, 2, 3]
+
+
+const lengthOfLongestSubstring = (str) => {
+    const charSet = new Set();
+    let left = 0;
+    let maxLength = 0;
+
+    for (let right = 0; right < str.length; right++) {
+        while (charSet.has(str[right])) {
+            charSet.delete(str[left]);
+            left++;
+        }
+        charSet.add(str[right]);
+        maxLength = Math.max(maxLength, right - left + 1);
+    }
+
+    return maxLength;
+}
+
+console.log(lengthOfLongestSubstring('pwwkew')); // 3
+
+
 console.log(
-    testData.groupBy(item => item.age)
-);
+    Temporal.Now.zonedDateTimeISO().toString(),    
+)
